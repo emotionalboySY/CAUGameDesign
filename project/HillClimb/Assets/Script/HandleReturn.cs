ddeb7856b11ab7a1590e0caa7f8ca0d22c6d5aaf
SeoHyunJong <shj971219@gmail.com>
2022-11-17 22:28:46 +0900

    그랩시 팔 추가

--- project/HillClimb/Assets/Script/HandleReturn.cs
+++ project/HillClimb/Assets/Script/HandleReturn.cs
@@ -7,17 +7,23 @@ public class HandleReturn : MonoBehaviour
 {
     public GameObject origin;
     public GameObject controller;
+    public GameObject oculusTouch;
+    public GameObject hand;
     public bool Activate = false;
 
     public void Attach()
     {
         controller.GetComponent<XRInteractorLineVisual>().enabled = false;
+        oculusTouch.SetActive(false);
+        hand.SetActive(true);
         Activate = true;
     }
 
     public void Return()
     {
         controller.GetComponent<XRInteractorLineVisual>().enabled = true;
+        oculusTouch.SetActive(true);
+        hand.SetActive(false);
         Activate = false;
         gameObject.transform.position = origin.transform.position;
         gameObject.transform.rotation = origin.transform.rotation;
