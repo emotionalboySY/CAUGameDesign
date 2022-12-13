4efdf90ebbcc58ec938191d3e5a01b9ba32a1598
Emotionalboy_SY <yoppyopp@naver.com>
2022-12-14 02:51:25 +0900

    Stage 1과 Practice에 남아있던 Screen UI 제거, VR 정상 작동 확인

--- project/HillClimb/Assets/Script/TimeManager.cs
+++ project/HillClimb/Assets/Script/TimeManager.cs
@@ -2,32 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TimeManager : MonoBehaviour
 {
-    Text timeTxt;
-    public float maxTime = 120f;
+    public TMP_Text timeTxt;
     public float time;
-    public float remainingTime;
     PlayerController player;
 
     // Start is called before the first frame update
     void Start()
     {
-        timeTxt = GetComponent<Text>();
         player = GameObject.Find("Player").GetComponent<PlayerController>();
-        time = Time.timeSinceLevelLoad;
     }
 
     // Update is called once per frame
     void Update()
     {
-        time = Time.timeSinceLevelLoad;
-        remainingTime = maxTime - time;
-        if(remainingTime <= 0) {
+        if(time <= 0) {
             player.GameOver();
         } else {
-            timeTxt.text = Mathf.Floor(remainingTime).ToString();
+            time -= Time.deltaTime;
+            timeTxt.text = Mathf.Floor(time).ToString();
         }
     }
 }
